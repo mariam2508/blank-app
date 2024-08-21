b79e251bf766b9591d32b5a802749ddad9cca9db
mariam2508 <mariamhossam243@gmail.com>
2024-08-21 22:56:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Car")
 Year=st.number_input('Year',min_value=0,max_value=10,value=1)
 Present_Price=st.number_input('Present_Price',min_value=0,max_value=10,value=1)
-output=model.predict([[Present_Price,Year]])
+output=model.predict([[Year,Present_Price]])
 st.write('car is',output[0][0])
 
 
